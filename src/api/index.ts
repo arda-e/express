06e6beac1f37dde1cbea46ec7755c682a9cc1b9e
Meric Arda Eren <aeren@netas.com.tr>
2023-10-07 16:43:25 +0300

    Add morgan, cors, helmet & add test for api/v1

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,14 @@
 import express from 'express';
 
+import {MessageResponse} from '../interfaces/';
+
 const router = express.Router();
+
+router.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: 'API - Root',
+  });
+});
 
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
-require('dotenv').config();
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+require('dotenv').config();
+
 import api from './api';
+import {MessageResponse} from './interfaces/';
 
 const app = express();
 
-interface MessageResponse {
-    message: string;
-}
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get<{}, MessageResponse>('/', (req, res) => {
-    res.json({
-      message: 'Welcome to Express API',
-    });
+  res.json({
+    message: 'Welcome to Express API' 
   });
+});
 
-app.use('api/v1', api)
+app.use('/api/v1', api);
 
-// Error handling middleware
 
 export default app;
